82ee529f5f6dd821e85f432d1c2bff8bd86a3cf9
Kim Mathieu <lockettks@gmail.com>
2019-03-31 00:49:28 -0400

    Cleanup and comment out possibly useless function closeStuff()

--- BootFinder/Components/InitialViewControllers.swift
+++ BootFinder/Components/InitialViewControllers.swift
@@ -85,15 +85,12 @@ class InitialViewController: UIViewController {
         let orientation   = CGImagePropertyOrientation(rawValue: UInt32(image.imageOrientation.rawValue))!
         guard let ciImage = CIImage(image: image) else { fatalError("Bad image") }
         
-        
         let model = try! VNCoreMLModel(for: self.imageSimilarity.model)
         
         let request = VNCoreMLRequest(model: model) {[weak self] request, error in
             self?.processQuery(for: request, error: error)
         }
         request.imageCropAndScaleOption = .centerCrop
-        
-        
         
         DispatchQueue.global(qos: .userInitiated).async {
             let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
@@ -150,26 +147,12 @@ class InitialViewController: UIViewController {
             }
             print (message)
             self.showSimilarities(message: message)
-            //            self.closeStuff()
-            
-            
         }
     }
     
     func getMetaData(topResults: ArraySlice<(offset: Int, element: Double)>){
-        
-        //        topResults.forEach {
-        //            var result : BootResult
-        ////            result.description = metaData[$0.offset - 1]["Description"]
-        //            let test = metaData[$0.offset - 1]["Description"]
-        ////            let result = "Element: \($0.element)  Offset: \($0.offset)\n"
-        ////            bootResults.append(result)
-        //        }
-        
-        for (index, element) in topResults.enumerated() {
-            var result = BootResult()
-            //            let test = metaData[element.offset - 1]
-            //            print(test)
+        for element in topResults.enumerated() {
+            let result = BootResult()
             result.description = metaData[element.offset - 1]["Description"].stringValue
             result.imageURL = metaData[element.offset - 1]["thumb_image"].stringValue
             result.price = metaData[element.offset - 1]["price"].stringValue
@@ -178,23 +161,23 @@ class InitialViewController: UIViewController {
         }
     }
     
-    func closeStuff() {
-        if let imagePicker = (self.presentedViewController as? UIImagePickerController) {
-            if (imagePicker.sourceType == .camera) {
-                self.dismiss(animated: false, completion: nil)
-            }
-        }
-        
-        // Save to Photo Library
-        if let imagePicker = (self.presentedViewController as? UIImagePickerController) {
-            if (imagePicker.sourceType == .camera) {
-                let saveAction = UIAlertAction(title: "Save Image", style: .default) { (actionitem) in
-                    UIImageWriteToSavedPhotosAlbum(self.currentImage, nil, nil, nil)
-                    self.dismiss(animated: false, completion: nil)
-                }
-            }
-        }
-    }
+//    func closeStuff() {
+//        if let imagePicker = (self.presentedViewController as? UIImagePickerController) {
+//            if (imagePicker.sourceType == .camera) {
+//                self.dismiss(animated: false, completion: nil)
+//            }
+//        }
+//
+//        // Save to Photo Library
+//        if let imagePicker = (self.presentedViewController as? UIImagePickerController) {
+//            if (imagePicker.sourceType == .camera) {
+//                let saveAction = UIAlertAction(title: "Save Image", style: .default) { (actionitem) in
+//                    UIImageWriteToSavedPhotosAlbum(self.currentImage, nil, nil, nil)
+//                    self.dismiss(animated: false, completion: nil)
+//                }
+//            }
+//        }
+//    }
     
     
     func showSimilarities(message:String) {
@@ -215,16 +198,9 @@ class InitialViewController: UIViewController {
         
         let action = UIAlertAction(title: "OK", style: .default) { (actionitem) in
             if let imagePicker = (self.presentedViewController as? UIImagePickerController) {
-//                if (imagePicker.sourceType == .camera) {
                     self.dismiss(animated: false, completion: {
                         self.performSegue(withIdentifier: "resultsSegue", sender: self)
-                        //                                            self.doTheSegue()
-                        
-                        //                    let storyBoard: UIStoryboard = UIStoryboard(name: "Storyboard", bundle: nil)
-                        //                    let vc = storyBoard.instantiateViewController(withIdentifier: "nvc") as! UINavigationController
-                        //                    self.present(vc, animated: true, completion: nil)
                     })
-//                }
             }
         }
         alertController.addAction(action)
@@ -265,7 +241,7 @@ extension InitialViewController: UIImagePickerControllerDelegate, UINavigationCo
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             self.similarifyImage(image: image)
-        }else{
+        } else{
             print("Something went wrong")
             self.dismiss(animated: true, completion: nil)
         }
